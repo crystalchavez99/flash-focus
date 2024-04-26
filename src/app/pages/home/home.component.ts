b6e59d71ba5b18aa83e814c7ed4fa660b688f421
Crystal Chavez <chavezcrystal1999@yahoo.com>
2024-04-25 23:03:39 -0700

    layout page is set with nav, home page contains banner and feature section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [ImageModule, ButtonModule]
 })
 export class HomeComponent {
 
