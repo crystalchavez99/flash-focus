f92206eeffb00295da3bb705597a693d1a195055
Crystal Chavez <chavezcrystal1999@yahoo.com>
2024-05-23 11:23:14 -0700

    gitignore

--- src/environments/environment.ts
+++
@@ -1,2 +0,0 @@
-export const supabaseUrl = 'https://ornhrekzrrwexxuekbjt.supabase.co';
-export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ybmhyZWt6cnJ3ZXh4dWVrYmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY0ODAzNDUsImV4cCI6MjAzMjA1NjM0NX0.928c7xqMEcAGwrHstkutRFIzSQX0OBmneXrCSjYRt9Y';
