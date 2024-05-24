4cfe0184924832bb530f48d552779cf3a5bb1e73
Crystal Chavez <chavezcrystal1999@yahoo.com>
2024-05-23 21:35:24 -0700

    profile page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -10,6 +10,25 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+
+  user:any;
+  userPic: any;
   constructor(public auth: AuthService) {console.log(auth.user$)}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.user = profile)
+    );
+  }
+
+  isGitHubUser(): boolean {
+    this.userPic = this.user.picture;
+    return this.user && this.user.sub.includes('github');
+  }
+
+  isGoogleUser(): boolean {
+    this.userPic = this.user.picture;
+    return this.user && this.user.sub.includes('google-oauth2');
+  }
 
 }
